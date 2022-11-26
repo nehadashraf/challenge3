95aac1fda5d4c885aabf2b60cbedc4283be5e418
nehadashraf <nehadashraf402@gmail.com>
2022-11-26 04:14:05 +0200

    first commit

---
+++ assets/JS/app.js
@@ -0,0 +1,11 @@
+jQuery("#responsive_headline").fitText();
+wow = new WOW(
+    {
+    boxClass:     'animate__animated',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
